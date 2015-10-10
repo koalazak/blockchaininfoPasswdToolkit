185f6ece646182656cf1736552c2e14a2e33751a
koalazak <facu@Aguacate.local>
2015-10-10 17:42:36 -0300

    explain iterations

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var check_password=require("./lib/cryptomagic.js");
 
 var payload=''; //payload value from https://blockchain.info/wallet/[[your_identifier]]?format=json
 var pass='';    //pass to test
-var iterations=0;
+var iterations=0; // set to 0 for older identifiers (test with 1 to 20 iterations). Use 5000 for new identifiers.
 
 if(check_password(payload, pass, iterations)) {
 	console.log("=======================");
