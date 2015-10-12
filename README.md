bb8682c54569f2d7e3938a3602e0a38cbe230887
koalazak <zak.tux@gmail.com>
2015-10-12 20:33:29 -0300

    getting payload from api

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ If you forgot your password, with this script you can try and re-try passwords, 
 In the near future, the script will bruteforce passwords.
 
 ## TODO
-  - Get identifier's payload via API
+  - ~~Get identifier's payload via API~~
+  - ~~Load payload from a file~~
   - Bruteforce mode
   - ~~Infinite re-try mode~~
 
@@ -17,13 +18,31 @@ $ cd blockchaininfoPasswdToolkit
 $ npm install
 ```
 
-### Run
-
-- Search your identifier payload here: https://blockchain.info/wallet/[[your_identifier]]?format=json
-- Edit app.js to fill your payload and then run:
+### Usage
 
 ```sh
-$ node app.js
+
+  Usage: app (-p <file ...> | -i <identifier>) [options]
+
+  A tool for test passwords on a blockchain.info wallet
+
+  Options:
+
+    -h, --help                     output usage information
+    -V, --version                  output the version number
+    -p, --payload <file ...>       read payload from a file
+    -i, --identifier <identifier>  get payload via API for that identifier
+    -d, --dictionary <file ...>    check each password on this file
+    -n, --iterations <number>      fix number of pbkdf2/iso7816 iterations. By Default check 1 to 20 and 5000
+
+  Examples:
+
+    $ app -p mypayload.txt
+    $ app -i "8ea09594-830c-4681-9d9e-6fe4fe7d8be6"
+    $ app -i "8ea09594-830c-4681-9d9e-6fe4fe7d8be6" -d words.txt
+    $ app -p mypayload.txt -n 5000
+    $ app -p mypayload.txt -n 5000 -d wordlist.txt
+
 ```
 
 ### License
--- app.js
+++ app.js
@@ -1,51 +1,198 @@
 var prompt = require('prompt');
+var program = require('commander');
 var check_password=require("./lib/cryptomagic.js");
 
-var payload=''; //payload value from https://blockchain.info/wallet/[[your_identifier]]?format=json
-var iterations=0; // set to 0 for older identifiers (this test with 1 to 20 iterations). Use 5000 for new identifiers.
+var iterations=0; // default set to 0 for older identifiers (this test with 1 to 20 and 5000 pbkdf2/iso7816 iterations).
 
-if(!payload){
-	console.log("Please edit app.js to fill 'payload' variable.");
-	process.exit();
+
+program
+  .version('0.9.1')
+  .description('A tool for test passwords on a blockchain.info wallet')
+  .usage('(-p <file ...> | -i <identifier>) [options]')
+  .option('-p, --payload <file ...>', 'read payload from a file')
+  .option('-i, --identifier <identifier>', 'get payload via API for that identifier')
+  .option('-d, --dictionary <file ...>', 'check each password on this file')
+  .option('-n, --iterations <number>', 'fix number of pbkdf2/iso7816 iterations. By Default check 1 to 20 and 5000');
+  
+  program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ app -p mypayload.txt');
+  console.log('    $ app -i "8ea09594-830c-4681-9d9e-6fe4fe7d8be6"');
+  console.log('    $ app -i "8ea09594-830c-4681-9d9e-6fe4fe7d8be6" -d words.txt');
+  console.log('    $ app -p mypayload.txt -n 5000');
+  console.log('    $ app -p mypayload.txt -n 5000 -d wordlist.txt');
+  console.log('');
+  });
+  program.parse(process.argv);
+
+
+
+if(typeof program.payload === "undefined" && typeof program.identifier === "undefined"){
+
+	showError("No -p or -i parameters given! Please specify one of those");
+	
+}else if(typeof program.payload != "undefined" && typeof program.identifier != "undefined"){
+
+	showError("You can not spcified two sources of payload. Just use -p OR -i parameters.");
+
 }
 
-prompt.message = "";
-prompt.delimiter = "";
-prompt.colors = false;
-prompt.start();
+if(typeof program.iterations != "undefined"){
 
-
-(function askPassword(){
-
-	var promptSchema = {
-	    properties: {
-	     password: {
-	        message:"Password:",
-	        hidden: true
-	      }
-	    }
-	};
-	  
-	prompt.get(promptSchema, function (err, result) {
+	iterations =  isNaN(parseInt(program.iterations)) ? 0 : parseInt(program.iterations) ;
 	
-		if(err){ //normally ctrl+c
-			console.log(err.message);
-			process.exit();
-		}
-		    
-	    if(check_password(payload, result.password, iterations)) {
-			console.log("=======================");
-		    console.log("Password OK: "+result.password);
-		    console.log("=======================");
-		}else{
-			console.log("Wrong password. Try again.");
-			//re-try forever
-			askPassword();
-		}
-	    
-	});
+	console.log("Using "+(iterations==0?"1 to 20 and 5000":iterations)+" pbkdf2/iso7816 iterations.");
+}
 
-})();
-  
-  
+
+if(typeof program.payload != "undefined"){
+	//read file
+	console.log("Reading payload from file...");
+	try{
+	
+		var payload=require("fs").readFileSync(program.payload).toString().trim();
+		if(payload){
+			console.log("Payload loaded OK.");
+			console.log("Starting 're-try forever' mode.");
+			startCrack(payload, iterations);
+		}else{
+			showError("Payload not found.");
+		}
+		
+		
+	}catch(e){
+	
+		showError(e.message);
+		
+	}
+	
+}else{
+	//get payload via api
+	var url='https://blockchain.info/wallet/'+program.identifier+'?format=json';
+	
+	console.log("Getting payload from: "+url);
+	
+	var request = require('request');
+	
+	request('https://blockchain.info/wallet/'+program.identifier+'?format=json', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    
+	    try{
+		    var payloadData=JSON.parse(body);
+		    
+		    if("payload" in payloadData){
+			    
+			    //TODO: Check authType?
+			    
+			    try{
+			    
+			    	var payloadNEWFormat=JSON.parse(payloadData.payload);
+			    	
+			    	if("payload" in payloadNEWFormat){
+			    	
+			    		var finalPayload=payloadNEWFormat.payload.trim();
+			    	
+				    	if("pbkdf2_iterations" in payloadNEWFormat){
+					    	if(iterations != payloadNEWFormat.pbkdf2_iterations){
+					    		iterations=payloadNEWFormat.pbkdf2_iterations;
+						    	console.log("Now Using API pbkdf2/iso7816 iteration value: "+iterations);
+					    	}
+				    	}
+			    	
+			    	}else{
+				    	 showError("Error getting payload. Bad JSON payload value.");
+			    	}
+			    	
+		    	}catch(e2){
+			    	var finalPayload=payloadData.payload.trim();
+		    	}
+		    	
+		    	if(finalPayload){
+			    	console.log("Payload loaded OK.");
+			    	startCrack(finalPayload, iterations);
+		    	}else{
+			    	 showError("Error getting payload. Bad JSON payload value. Is two-factor auth enabled?");
+		    	}
+			    
+		    }else{
+			    showError("Error getting payload. Bad JSON payload value. Is two-factor auth enabled?");
+		    }
+		    
+	    }catch(e){
+	    	console.log(e.message);
+	    	showError("Error getting payload. Bad JSON.");
+	    }
+	    
+	  }else{
+	      console.error(error,response.statusCode);
+	  	  showError("Error getting payload. Bad identifier?");
+		  
+	  }
+	})
+
+}
+
+
+function showError(msg){
+	program.outputHelp();
+	console.error('  Error:');
+	console.error('');
+	console.error('  '+msg);
+	console.error('');
+	process.exit(1);
+}
+
+function startCrack(payload, iterations){
+	
+	if(typeof program.dictionary != "undefined"){
+	
+		//dictionary mode
+		console.log("TODO");
+		process.exit();
+	
+	}else{
+		
+		
+		prompt.message = "";
+		prompt.delimiter = "";
+		prompt.colors = false;
+		prompt.start();
+		
+		(function askPassword(){
+		
+			var promptSchema = {
+			    properties: {
+			     password: {
+			        message:"Password:",
+			        hidden: true
+			      }
+			    }
+			};
+			  
+			prompt.get(promptSchema, function (err, result) {
+			
+				if(err){ //normally ctrl+c
+					console.log(err.message);
+					process.exit();
+				}
+				    
+			    if(check_password(payload, result.password, iterations)) {
+					console.log("=======================");
+				    console.log("Password OK: "+result.password);
+				    console.log("=======================");
+				}else{
+					console.log("Wrong password. Try again.");
+					//re-try forever
+					askPassword();
+				}
+			    
+			});
+		
+		})();
+
+	} //re-try forever mode
+	
+}
+
 
