415fdd299b806ad604b63b6ffed00aee034508a6
koalazak <zak.tux@gmail.com>
2015-10-11 21:46:42 -0300

    re-try mode

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# blockchain.info Password toolkit
+# blockchain.info recovery password toolkit
 
 A script to off-line test passwords in a blockchain.info wallet.
+If you forgot your password, with this script you can try and re-try passwords, indefinitely.
+In the near future, the script will bruteforce passwords.
 
 ## TODO
   - Get identifier's payload via API
   - Bruteforce mode
-  - Infinite re-try mode
+  - ~~Infinite re-try mode~~
 
 ### Installation
 
@@ -18,7 +20,7 @@ $ npm install
 ### Run
 
 - Search your identifier payload here: https://blockchain.info/wallet/[[your_identifier]]?format=json
-- Edit app.js to put your password and payload and then:
+- Edit app.js to fill your payload and then run:
 
 ```sh
 $ node app.js
--- app.js
+++ app.js
@@ -1,13 +1,51 @@
+var prompt = require('prompt');
 var check_password=require("./lib/cryptomagic.js");
 
 var payload=''; //payload value from https://blockchain.info/wallet/[[your_identifier]]?format=json
-var pass='';    //pass to test
-var iterations=0; // set to 0 for older identifiers (test with 1 to 20 iterations). Use 5000 for new identifiers.
+var iterations=0; // set to 0 for older identifiers (this test with 1 to 20 iterations). Use 5000 for new identifiers.
 
-if(check_password(payload, pass, iterations)) {
-	console.log("=======================");
-    console.log("Password OK: "+pass);
-    console.log("=======================");
-}else{
-	console.log("Invalid password.");
+if(!payload){
+	console.log("Please edit app.js to fill 'payload' variable.");
+	process.exit();
 }
+
+prompt.message = "";
+prompt.delimiter = "";
+prompt.colors = false;
+prompt.start();
+
+
+(function askPassword(){
+
+	var promptSchema = {
+	    properties: {
+	     password: {
+	        message:"Password:",
+	        hidden: true
+	      }
+	    }
+	};
+	  
+	prompt.get(promptSchema, function (err, result) {
+	
+		if(err){ //normally ctrl+c
+			console.log(err.message);
+			process.exit();
+		}
+		    
+	    if(check_password(payload, result.password, iterations)) {
+			console.log("=======================");
+		    console.log("Password OK: "+result.password);
+		    console.log("=======================");
+		}else{
+			console.log("Wrong password. Try again.");
+			//re-try forever
+			askPassword();
+		}
+	    
+	});
+
+})();
+  
+  
+
